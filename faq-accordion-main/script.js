2436bce720ee1578949a18b0abf1dab7e7b98d48
Eavelie <athallahnaufalr@gmail.com>
2023-12-26 13:14:34 +0700

    added faq accordion project

---
+++ faq-accordion-main/script.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMContentLoaded', () => {
+    let accordion = document.getElementsByClassName("accordion");
+    for (let i = 0; i < accordion.length; i++) {
+        accordion[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+
+            let panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        })
+    }
+})
