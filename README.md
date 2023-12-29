30a51b25db4684f6409bb3eab06efcecafc911cb
Eavelie <athallahnaufalr@gmail.com>
2023-12-29 14:22:28 +0700

    project reinitialization, modified base layouts

---
+++ README.md
@@ -0,0 +1,2 @@
+# Frontend Mentor Solutions
+This is a repository of my solutions to the challenges provided by Frontend Mentor.
--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -1,15 +1,15 @@
-window.addEventListener('DOMContentLoaded', () => {
-    let accordion = document.getElementsByClassName("accordion");
-    for (let i = 0; i < accordion.length; i++) {
-        accordion[i].addEventListener("click", function () {
-            this.classList.toggle("active");
+let accordionButtons = document.getElementsByClassName("accordion-item-button");
 
-            let panel = this.nextElementSibling;
-            if (panel.style.display === "block") {
-                panel.style.display = "none";
-            } else {
-                panel.style.display = "block";
-            }
-        })
-    }
-})
+let i;
+for (i = 0; i < accordionButtons.length; i++) {
+    accordionButtons[i].addEventListener("click", function()  {
+        this.classList.toggle("active");
+
+        let accordionPanel = this.nextElementSibling;
+        if (accordionPanel.style.display === "block") {
+            accordionPanel.style.display = "none";
+        } else {
+            accordionPanel.style.display = "block";
+        }
+    })
+}
